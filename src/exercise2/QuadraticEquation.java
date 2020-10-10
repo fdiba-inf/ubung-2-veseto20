4238e29f8543e64c4f767a5bcf9d6817f66634ef
veseto20 <vesela_krumova2001@abv.bg>
2020-10-10 21:21:57 +0000

    done

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 public class QuadraticEquation {
   public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
-    int a = input.nextInt();
-    int b = input.nextInt();
-    int c = input.nextInt();
-    if (a!=0){
+    double a = input.nextInt();
+    double b = input.nextInt();
+    double c = input.nextInt();
+    double x1;
+    double x2;
+    double D = Math.pow (b,2)-4*a*c;
 
+    if (a!=0){
+      System.out.println("Berechnung:" + D);
+    }
+
+    if (D>=0){
+
+       x1=-b+Math.sqrt(D)/2*a;
+       x2=-b-Math.sqrt(D)/2*a;
+       System.out.println("x1 =" + x1
+       );
     }
   }
 }  
