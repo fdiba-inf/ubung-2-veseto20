9b46ee597d48b36cad25ba0eacaee17b239054af
veseto20 <vesela_krumova2001@abv.bg>
2020-10-11 20:42:05 +0000

    done

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -6,36 +6,37 @@ public class QuadraticEquation {
   public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
     double a = input.nextInt();
-    System.out.println("a = " + a);
+    System.out.println("Enter a value for a" + a);
     double b = input.nextInt();
-    System.out.println("b = " + b);
+    System.out.println("Enter a value for b = " + b);
     double c = input.nextInt();
-    System.out.println("c = " + c);
+    System.out.println("Enter a value for c = " + c);
 
      if (a!=0){
-      double D = Math.pow (b,2)-4*a*c;
+      double D = Math.pow (b,2)-(4*a*c);
       System.out.println("D = " + D);
     }
 
     if (D>=0){
 
-       double x1=-b+Math.sqrt(D)/2*a;
+       double x1=(-b+Math.sqrt(D))/(2*a);
        System.out.println("x1 =" + x1);
-       double x2=-b-Math.sqrt(D)/2*a;
+       double x2=(-b-Math.sqrt(D))/(2*a);
        System.out.println("x2 =" + x2);
-
-    } else {
+  } 
+    else {
       System.out.println("Imaginary values");
     }
+
   }
     else if (b!=0){
       double x=(-c)/b;
       System.out.println("x =" + x);
     }
-    else if (c!=0) {
+    else if (b==0 & c!=0) {
       System.out.println("No values");
     }
-     else (c==0) {
+     else (b==0 & c==0) {
        System.out.println("Many values");
      }
 }
